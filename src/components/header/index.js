b2186fa32470536a203f0c7363efad9dd728efaa
Tom Walsh <hello@tmwlsh.co.uk>
2021-07-25 14:54:36 +0100

    feat: header content laid out

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+import * as styles from "./styles.module.scss";
+
 const Header = () => {
+
+  const slideToBeers = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={styles.header}>
+      <p className={styles.headerScrollText}>Premium Lagers</p>
+      <p className={styles.headerScrollText}>Delicious Ales</p>
+      <div className={styles.headerContent}>
+        <h1 className={styles.headerTitle}>Cold<span>Ones</span></h1>
+        <button
+          onClick={e => slideToBeers(e)}
+          className={styles.headerCta}
+        >
+          View Brewskies
+        </button>
+      </div>
     </header>
   )
 }
